27a6ca78860b278bfbc0e2417e2851518403a309
Faizan Ali <fali.abdulali@gmail.com>
2025-02-16 15:59:20 +0500

    add session to admin

--- nigotis/chatbot/admin.py
+++ nigotis/chatbot/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
-from .models import Client, Message
+from .models import Client, Session, Message
 
 
 @admin.register(Client)
-class ChatSessionAdmin(admin.ModelAdmin):
+class ClientAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "role", "login_email")
 
 
+@admin.register(Session)
+class SessionAdmin(admin.ModelAdmin):
+    list_display = ("id", "phone_number", "website", "client")
+
+
 @admin.register(Message)
-class ChatMessageAdmin(admin.ModelAdmin):
+class MessageAdmin(admin.ModelAdmin):
     list_display = ("id", "sender", "content", "session")
--- nigotis/chatbot/models.py
+++ nigotis/chatbot/models.py
@@ -20,15 +20,22 @@ class Client(BaseModel):
     name = models.CharField(max_length=255)
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
     login_email = models.EmailField(null=True, blank=True)
-    login_password = models.CharField(max_length=255,null=True, blank=True)
+    login_password = models.CharField(max_length=255, null=True, blank=True)
     auth_token = models.CharField(max_length=255, null=True, blank=True)
     authenticated_at = models.DateTimeField(null=True, blank=True)
 
+
 class Session(BaseModel):
-    
-    phone_number = models.CharField(max_length=20, null=True, blank=True)  
-    website=models.BooleanField(default=False)
-    client = models.ForeignKey(Client, on_delete=models.SET_NULL, null=True, blank=True, related_name="sessions")
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    website = models.BooleanField(default=False)
+    client = models.ForeignKey(
+        Client,
+        null=True,
+        blank=True,
+        related_name="sessions",
+        on_delete=models.SET_NULL,
+    )
+
 
 class Message(BaseModel):
     SENDER_CHOICES = [
@@ -38,17 +45,9 @@ class Message(BaseModel):
 
     sender = models.CharField(max_length=10, choices=SENDER_CHOICES)
     content = models.TextField()
-    unique_message_id=models.TextField()
+    unique_message_id = models.TextField()
     session = models.ForeignKey(
         Session,
         related_name="messages",
         on_delete=models.CASCADE,
     )
-
-
-
-
- 
-
-
-
