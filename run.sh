c4401966b780df7cac5cea30492bd80b06bcdd0d
Faizan Ali <fali.abdulali@gmail.com>
2025-04-27 01:04:49 +0500

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 git fetch
 git pull origin
+pip install -r requirements.txt
 
 nohup python3 nigotis/manage.py runserver 0.0.0.0:8000 &
 nohup ngrok http --domain=one-urchin-ultimate.ngrok-free.app 8000 &
