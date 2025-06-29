ca387fe8fca661f253fdb2671757ad2dee33b5e5
Faizan Ali <fali.abdulali@gmail.com>
2025-06-30 01:10:06 +0500

    switch to Twilio

--- nigotis/whatsapp/views.py
+++ nigotis/whatsapp/views.py
@@ -3,69 +3,8 @@ from django.views.decorators.csrf import csrf_exempt
 from twilio.twiml.messaging_response import MessagingResponse
 
 from agent.agent import ToolAgent
-from .service import WhatsAppService
+from chatbot.models import Session
 from memory.manager import MemoryManager
-from chatbot.models import Message, Session
-
-
-# @csrf_exempt
-# def webhook(request):
-#     memory_manager = MemoryManager()
-#     whatsapp_service = WhatsAppService()
-
-#     input_payload = whatsapp_service.receive_message(request)
-
-#     if isinstance(input_payload, (HttpResponse, JsonResponse)):
-#         return input_payload
-
-#     sender_id = input_payload["sender_id"]
-#     incoming_text = input_payload["incoming_text"]
-#     unique_message_id = input_payload["unique_message_id"]
-
-#     already_exists = Message.objects.filter(
-#         unique_message_id=unique_message_id
-#     ).exists()
-
-#     print(f"Message: {input_payload}")
-#     print(f"Already exists: {already_exists}")
-
-#     if already_exists:
-#         return JsonResponse(
-#             {
-#                 "status": "success",
-#                 "message": "message already exists",
-#             },
-#             status=200,
-#         )
-
-#     session, _ = Session.objects.get_or_create(phone_number=sender_id)
-
-#     memory_manager.add_message(
-#         sender="USER",
-#         content=incoming_text,
-#         session_id=session.id,
-#         unique_message_id=unique_message_id,
-#     )
-
-#     agent = ToolAgent()
-
-#     bot_response = agent.get_response(session, incoming_text)
-
-#     whatsapp_service.send_message(to=sender_id, what=bot_response)
-
-#     memory_manager.add_message(
-#         sender="BOT",
-#         content=bot_response,
-#         session_id=session.id,
-#     )
-
-#     return JsonResponse(
-#         {
-#             "status": "success",
-#             "message": "message sent",
-#         },
-#         status=200,
-#     )
 
 
 @csrf_exempt
